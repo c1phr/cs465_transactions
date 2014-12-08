b25d12ab2fa83c5efeea6361893a02cf8299f245
Ryan Batchelder <rb688@nau.edu>
2014-12-08 13:10:12 -0700

    Cleaning up some syntax errors and adding stop conditions

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,6 @@
 from time import sleep
-from random import randint
+from random import randint, random
+
 
 class Agent(object):
     """ 
@@ -9,16 +10,16 @@ class Agent(object):
     """
     def __init__(self, num_cubbies, locking_enabled):
         self.locking_enabled=locking_enabled
+        self.num_cubbies = num_cubbies
 
-    def modify_cubbyhole(cubby_a, cubby_b, delta_upper_bound=50
-            agents_array):
+    def modify_cubbyhole(self, cubby_a, cubby_b, delta_upper_bound=50):
         # The calling process should indicate which cubbyholes to modify,
         # but the Agent will automatically select a value by which to change
         # the cubbyholes' values.
-        delta_value_a = random.randint(0, delta_upper_bound)
+        delta_value_a = randint(0, delta_upper_bound)
         delta_value_b = delta_value_a * (-1)
 
-        if locking_enabled:
+        if self.locking_enabled:
             while cubby_a.lock_status() or cubby_b.lock_status() == True:
                 sleep(0.001)
             cubby_a.lock()
@@ -32,10 +33,8 @@ class Agent(object):
             cubby_a.change_value(delta_value_a)
             cubby_b.change_value(delta_value_b)
 
-    def run(cubby_array):
+    def run(self, cubby_array):
         while True:
-            random_cubby_a = cubby_array[random.randint(0,
-                self.num_cubbies)]
-            random_cubby_b = cubby_array[random.randint(0,
-                self.num_cubbies)]
-            modify_cubbyhole(random_cubby_a, random_cubby_b)
+            random_cubby_a = cubby_array[randint(0, self.num_cubbies)]
+            random_cubby_b = cubby_array[randint(0, self.num_cubbies)]
+            self.modify_cubbyhole(random_cubby_a, random_cubby_b)
--- src/manager.py
+++ src/manager.py
@@ -1,5 +1,6 @@
 import multiprocessing
 from random import randint
+import time
 from src.agent import Agent
 from src.cubby import Cubby
 
@@ -10,9 +11,7 @@ class Manager(object):
     system, and it maintains an array of cubbyholes, an array of agents, and
     other information about system state.
     """
-    def __init__(self, num_cubbies, max_initial_cubby_value,
-            locking_enabled=True, terminate_on="transactions",
-            termination_point="10"):
+    def __init__(self, num_cubbies, initial_cubby_value, locking_enabled=True, terminate_on="transactions", termination_point="10"):
         # terminate_on specifies 'transactions' or 'time', and
         # termination_point specifies (depending on the value of
         # terminate_on) the number of transactions or seconds after which to
@@ -25,20 +24,28 @@ class Manager(object):
 
         self.cubbyholes = []
         for cubby in range(num_cubbies):
-            self.cubbyholes.append(Cubby(randint(0, max_cubby_value)))
+            self.cubbyholes.append(Cubby(initial_cubby_value))
 
         self.agents = []
-        for agent in range(num_agents):
+        for agent in range(self.num_agents):
             self.agents.append(Agent(locking_enabled=True))
 
         self.agent_processes = []
         
     def run(self):
         if self.terminate_on=="time":
-            for this_agent in self.agents:
-                this_agent_process = multiprocessing.Process(
-                        target=this_agent.run, args=(self.cubbyholes))
-                this_agent_process.start()
+            start_time = time.clock() # Get current time
+            end_time = start_time + self.termination_point
+            while time.clock() < end_time:
+                for this_agent in self.agents:
+                    this_agent_process = multiprocessing.Process(target=this_agent.run, args=(self.cubbyholes, ))
+                    this_agent_process.start()
 
-            time.sleep(self.termination_point)
+        else:
+            run_counter = 0
+            while run_counter < self.termination_point:
+                for this_agent in self.agents:
+                    this_agent_process = multiprocessing.Process(target=this_agent.run, args=(self.cubbyholes, ))
+                    this_agent_process.start()
+                    run_counter += 1
 
