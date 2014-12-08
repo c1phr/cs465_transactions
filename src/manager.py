6de1151b01168050bad7b78313a8f12d95a7baa0
Andrew Stebenne <ssonicblue@gmail.com>
2014-12-08 11:08:18 -0700

    Time-termination portion of manager complete, next up is number-of-transactions termination

--- src/manager.py
+++ src/manager.py
@@ -10,8 +10,9 @@ class Manager(object):
     system, and it maintains an array of cubbyholes, an array of agents, and
     other information about system state.
     """
-    def __init__(self, num_cubbies, max_cubby_value, locking_enabled=True,
-            terminate_on="transactions", termination_point="10"):
+    def __init__(self, num_cubbies, max_initial_cubby_value,
+            locking_enabled=True, terminate_on="transactions",
+            termination_point="10"):
         # terminate_on specifies 'transactions' or 'time', and
         # termination_point specifies (depending on the value of
         # terminate_on) the number of transactions or seconds after which to
@@ -29,12 +30,15 @@ class Manager(object):
         self.agents = []
         for agent in range(num_agents):
             self.agents.append(Agent(locking_enabled=True))
-        
-        self.pool = multiprocessing.Pool(processes=num_agents)
 
+        self.agent_processes = []
+        
     def run(self):
         if self.terminate_on=="time":
-            time_init = process_time()
             for this_agent in self.agents:
                 this_agent_process = multiprocessing.Process(
-                        target=this_agent.run, args=())
+                        target=this_agent.run, args=(self.cubbyholes))
+                this_agent_process.start()
+
+            time.sleep(self.termination_point)
+
