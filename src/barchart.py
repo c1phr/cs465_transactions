66a3a72128c24cbc1b720fe884aae84ab4bbe09e
Salvatore Bottiglieri <sb797@nau.edu>
2014-12-08 14:11:01 -0700

    Barchart updated to show what bar chart will look like. Animation and class structure coming soon

--- src/barchart.py
+++ src/barchart.py
@@ -1,4 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
-class Barchart(object):
+#will covert to class after test
 
-    def __init__(self):
+N = 5
+cubby_val   = (4, 4, 4, 4, 4)
+ind = np.arange(N)    # the x locations for the groups
+width = 0.35       # the width of the bars: can also be len(x) sequence
+
+p1 = plt.bar(ind, cubby_val,   width, color='g')
+
+plt.ylabel('Value')
+plt.title('Value of Cubbies')
+plt.xticks(ind+width/2., ('C1', 'C2', 'C3', 'C4', 'C5') )
+plt.yticks(np.arange(0,21,1))
+
+plt.show()
+
+
+
--- src/transaction.py
+++ src/transaction.py
@@ -3,3 +3,4 @@
 
 class Transaction(object):
     def __init__(self):
+        pass
