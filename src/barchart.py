1694e6d12274706d71cb2fe74ca34f829a6b9d22
Salvatore Bottiglieri <sb797@nau.edu>
2014-12-08 20:33:55 -0700

    Barchart almost complete but okay for testing

--- src/barchart.py
+++ src/barchart.py
@@ -1,21 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#will covert to class after test
+class barchart(object):
 
-N = 5
-cubby_val   = (4, 4, 4, 4, 4)
-ind = np.arange(N)    # the x locations for the groups
-width = 0.35       # the width of the bars: can also be len(x) sequence
+    def __init__(self, values):
+        self.value = values
 
-p1 = plt.bar(ind, cubby_val,   width, color='g')
+    def change_value(self, new_value):
+        self.value = new_value
 
-plt.ylabel('Value')
-plt.title('Value of Cubbies')
-plt.xticks(ind+width/2., ('C1', 'C2', 'C3', 'C4', 'C5') )
-plt.yticks(np.arange(0,21,1))
+    def make_barchart(self):
+        while True:
+            cubby_val   = self.value
+            N = len(cubby_val)
+            label_list = []
+            for i in range(0, N):
+                label_list.append("C" + str(i+1))
+            index = np.arange(N)    # the x locations for the groups
+            width = .8       # the width of the bars: can also be len(x) sequence
 
-plt.show()
+            p1 = plt.bar(index, cubby_val, width, color='b', edgecolor="r", align="center")
+
+            plt.ylabel('Value')
+            plt.title('Value of Cubbies')
+            plt.xticks(index, label_list)
+            plt.yticks(np.arange(0,22,1))
+
+            def autolabel(p1):
+                # attach some text labels
+                for rect in p1:
+                    height = rect.get_height()
+                    plt.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
+                            ha='center', va='bottom')
+
+            autolabel(p1)
+
+            plt.show()
 
 
 
